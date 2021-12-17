91cc7ea0ab876713e9942931e9917ff1f31b6afa
Yury_Liakhavets <yury_liakhavets@epam.com>
2021-12-17 18:21:19 +0300

    added mobx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { observer } from "mobx-react-lite";
 
-function App() {
+const App = () => {
   return (
     <>TODO</>
   );
 }
 
-export default App;
+export default observer(App);
